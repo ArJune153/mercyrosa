b22fa8924ee2d32f274af3e922a8d74d93a61515
mrdarklord005-creator <mrdarklord005@gmail.com>
2025-11-14 02:08:32 +0700

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,11 +6,11 @@
       <br><b class="text2">✨การันตีผู้ใช้งานจริงนับล้านยูสเซอร์ ลูกค้าใหม่ เพิ่มอัตราการชนะ ทันที !!</b>
     </div>
     <div class="button-container">
-    <a href="https://lin.ee/knMX628" class="custom-button1">
+    <a href="https://lin.ee/knMX628" class="custom-button1" onclick="trackSubscribe()">
         <i class="fas fa-user-plus"></i>  <!-- ไอคอนสมัครสมาชิก -->
         สมัครสมาชิก
     </a>
-    <a href="https://lin.ee/knMX628" class="custom-button2">
+    <a href="https://lin.ee/knMX628" class="custom-button2" onclick="trackSubscribe()">
         <i class="fas fa-comments"></i>  <!-- ไอคอนติดต่อแอดมิน -->
         ติดต่อแอดมิน
     </a>
@@ -185,6 +185,9 @@ export default {
     };
   },
   methods: {
+    trackSubscribe() {
+    fbq('track', 'Subscribe'); // ใช้เหตุการณ์ Subscribe เพื่อติดตามการสมัครสมาชิก
+   },
     createRankingCard(user, rank) {
         const rankingContainer = document.getElementById("rankingContainer");
 
